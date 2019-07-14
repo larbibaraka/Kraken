8f54ad4a98afd85a60cc864fe44cf79b3e1d7a0b
Chams AGOUNI <chamsagouni8@gmail.com>
2019-07-14 18:33:20 +0100

    Update Header Component

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,16 +6,22 @@ import logo from './logo.svg';
 class Header extends Component {
     render() {
         return (
-            <nav className="uk-navbar-container">
-                <div className="uk-navbar-left">
-                    <img src={logo} height="100px" width="100px" className="uk-navbar-item uk-logo"></img>
-                </div>
-                <div className="uk-navbar">
-                    <a className="uk-button uk-button-default uk-button-small" href="#">Link</a>
-                    <a className="uk-button uk-button-default uk-button-small" href="#">Link</a>
-                    <a className="uk-button uk-button-default uk-button-small" href="#">Link</a>
-                </div>
-            </nav>
+            <header>
+                <nav className="kr-app-header-topMenu">
+                    <div className="kr-app-header-topMenu-logo">
+                        <img className="logo" src={logo} alt="Kraken" width="150" height="100"/>
+                    </div>
+                    <p className="uk-align-center kr-app-header-topMenu-dashButtons">
+                        <a className="uk-button uk-button-default tabButton" href="#">Link</a>
+                        <a className="uk-button uk-button-default tabButton" href="#">Link</a>
+                        <a className="uk-button uk-button-default tabButton" href="#">Link</a>
+                    </p>
+                    <button className="uk-button uk-button-danger uk-text-bold uk-align-right">
+                        <span uk-icon="plus"></span>
+                        <span> New Task </span>
+                    </button>
+                </nav>
+            </header>
         )
     }
 }
