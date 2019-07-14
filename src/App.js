a8187dd2b7a311f9defb1d22360f7c59ec982017
Chams AGOUNI <chamsagouni8@gmail.com>
2019-07-14 18:30:06 +0100

    Add Sidebar Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Frame from './Components/Frame/Frame';
 import Header from './Components/Header/Header';
+import Sidebar from './Components/Sidebar/Sidebar';
 
 function App() {
   return (
     <div className="App">
-      <Frame />
       <Header />
+      <Sidebar />
     </div>
   );
 }
---
+++ src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Sidebar.scss';
+
+
+class Sidebar extends Component {
+    render() {
+        return (
+            <div className="kr-app-Sidebar">
+                123
+            </div>
+        )
+    }
+}
+
+export default Sidebar;
