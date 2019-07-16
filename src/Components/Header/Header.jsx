49503846ffca5911bee58b920c016856e95400f8
Chams AGOUNI <chamsagouni8@gmail.com>
2019-07-16 01:05:10 +0100

    Update Top Menu Links names

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -11,12 +11,12 @@ class Header extends Component {
                     <div className="kr-app-header-topMenu-logo">
                         <img className="logo" src={logo} alt="Kraken" width="150" height="100"/>
                     </div>
-                    <p className="uk-align-center kr-app-header-topMenu-dashButtons">
-                        <a className="uk-button uk-button-default tabButton" href="#">Link</a>
-                        <a className="uk-button uk-button-default tabButton" href="#">Link</a>
-                        <a className="uk-button uk-button-default tabButton" href="#">Link</a>
-                    </p>
-                    <button className="uk-button uk-button-danger uk-text-bold uk-align-right">
+                    <div className="kr-app-header-topMenu-dashButtons">
+                        <a className="uk-button uk-button-default tabButton" href="#">Board</a>
+                        <a className="uk-button uk-button-default tabButton" href="#">List</a>
+                        <a className="uk-button uk-button-default tabButton" href="#">Calendar</a>
+                    </div>
+                    <button className="uk-button uk-button-danger uk-text-bold">
                         <span uk-icon="plus"></span>
                         <span> New Task </span>
                     </button>
