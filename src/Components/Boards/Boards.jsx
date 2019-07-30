cddf24ca5737aa4e4b663605a43cd637cdb817dd
Chams AGOUNI <chamsagouni8@gmail.com>
2019-07-30 12:17:02 +0100

    Add Board Component

---
+++ src/Components/Boards/Boards.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './Boards.scss';
+
+class Board extends Component {
+    render() {
+        return (
+            <div className="kr-app-board">
+                <div className="kr-app-board-header">
+                    123
+                </div>
+                <div className="kr-app-board-content">
+                    123
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Board;
