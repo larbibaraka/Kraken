9589572b7d21ee6113f9b8f581ce5bb79e646bb7
Chams AGOUNI <chamsagouni8@gmail.com>
2019-08-01 00:26:57 +0100

    Update Boards Component to query graphql

--- src/Components/Boards/Boards.jsx
+++ src/Components/Boards/Boards.jsx
@@ -1,15 +1,47 @@
 import React, { Component } from 'react';
 import './Boards.scss';
+import { GET_COLUMNS } from '../../Requests';
+import { DragDropContext } from 'react-beautiful-dnd';
+import Column from './Column/Column';
+import { GraphQLClient } from "graphql-request";
 
 class Board extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            columns: []
+        }
+    }
+
+    getData() {
+        const client = new GraphQLClient("http://localhost:8000/graphql");
+
+        client.request(GET_COLUMNS, {"columnId": this.state.columnId, "first": this.state.first}).then(data => {
+            this.setState({
+                columns : data.columns
+            })
+        });
+    }
+
+    componentDidMount() {
+        this.getData()
+    }
+
     render() {
         return (
             <div className="kr-app-board">
                 <div className="kr-app-board-header">
-                    123
+
                 </div>
                 <div className="kr-app-board-content">
-                    123
+                    <div className="kr-app-boards-content-columns">
+                        {this.state.columns.map(column => (
+                            <DragDropContext key={column.id}>
+                                <Column key={column.id} column={column} />
+                            </DragDropContext>
+                            )
+                        )}
+                    </div>
                 </div>
             </div>
         )
