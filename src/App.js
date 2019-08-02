4ae742c119f8ac779621c8d55f575075a93c8503
laridev@gmail.com <laridev@gmail.com>
2019-08-03 00:05:28 +0100

    fix things :clap:

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Header from './Components/Header/Header';
 import Sidebar from './Components/Sidebar/Sidebar';
 import Boards from './Components/Boards/Boards';
-import Routes from './Routes'
+import Routes from './Routes';
 
 function App() {
 
